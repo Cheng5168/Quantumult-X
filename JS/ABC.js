8e041904e5dd1ee036df71bcef4da4090daad26f
Cheng5168 <158341129+Cheng5168@users.noreply.github.com>
2024-03-21 19:04:33 +0800

    Add files via upload

---
+++ JS/ABC.js
@@ -0,0 +1,24 @@
+{
+
+} else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
+  // 中国农业银行开屏广告
+  const listbankabc = [
+    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
+    "com.abchina.mbank.common.homepage.getStartParam"
+  ];
+  if (isQuanX) {
+    if (listbankabc?.includes(headopt)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (listbankabc?.includes(headopt)) {
+      $done();
+    } else {
+      $done({});
+    }
+  }
+} else {
+  $done({});
+}
