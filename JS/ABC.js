968dd36c88810df5a073df87fce74e4b8f17b807
Cheng5168 <158341129+Cheng5168@users.noreply.github.com>
2024-03-21 19:12:50 +0800

    ABC.js

--- JS/ABC.js
+++ JS/ABC.js
@@ -1,25 +1,35 @@
-{ 
-}  
-{
-    } else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
-      // 中国农业银行开屏广告
-      const listbankabc = [
-        "com.bankabc.recommendcenter.homepage.gethpadverinfo",
-        "com.abchina.mbank.common.homepage.getStartParam"
-      ];
-      if (isQuanX) {
-        if (listbankabc?.includes(headopt)) {
-          $done({ status: "HTTP/1.1 404 Not Found" });
-        } else {
-          $done({});
-        }
-      } else {
-        if (listbankabc?.includes(headopt)) {
-          $done();
-        } else {
-          $done({});
-        }
-      }
+  if (isQuanX) {
+    if (list12306?.includes(headopt)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
     } else {
       $done({});
     }
+  } else {
+    if (list12306?.includes(headopt)) {
+      $done();
+    } else {
+      $done({});
+    }
+  }
+} else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
+  // 中国农业银行开屏广告
+  const listbankabc = [
+    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
+    "com.abchina.mbank.common.homepage.getStartParam"
+  ];
+  if (isQuanX) {
+    if (listbankabc?.includes(headopt)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (listbankabc?.includes(headopt)) {
+      $done();
+    } else {
+      $done({});
+    }
+  }
+} else {
+  $done({});
+}
